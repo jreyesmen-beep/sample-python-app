d100306cd7a9152cc2ff9709fae3a568a335fc71
jreyesmen-beep <jreyesmen@gmail.com>
2026-03-27 20:16:20 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker docker pull jreyesdocker/simple-python-flask-app
+docker pull jreyesdocker/simple-python-flask-app:latset
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 jreyesdocker/simple-python-flask-app
+docker run -d -p 5000:5000 jreyesdocker/simple-python-flask-app:latset
